d067fbfa0f7c296a8fe3c90adc673d0ec40ad86d
quizitocristiano <quizitocristiano10@gmail.com>
2023-03-08 17:43:37 -0300

    adicionato o css no site

---
+++ index.js
@@ -0,0 +1,52 @@
+
+// let container = document.querySelector('#password')
+// let input = document.querySelector('input');
+// let icons = document.querySelector('img')
+
+
+
+// icons.addEventListener('click', function() {
+//     container.classList.toggle('visible');
+//     if ( container.classList.contains('visible')) {
+//         icons.src = 'asssets/eye-off.png';
+//         input.type = 'text';
+//     } 
+//     else {
+//         icons.src = 'asssets/eye.png';
+//         input.type = 'password'
+//     }
+    
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function viewSenha(){
+//     var tipo = document.getElementById("senha")
+//     if (tipo.type == "password") {
+//         tipo.type = "text";
+//     }else{
+//         tipo.type = "password";
+//     }
+
+
+//     console.log('Você está adivo')
+// }
