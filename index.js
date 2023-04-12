199b53c584773d7faa0affed51fc0cd8fe3b0e82
camotio02 <temotio.bernardo@snowmanlabs.com>
2023-04-12 15:05:54 -0300

    fazendo o refactor rules

--- index.js
+++ index.js
@@ -20,123 +20,123 @@ const validacao = () => {
     const today = new Date();
 
     // PEGANDO OS INPUTS DO FORM
-    const dada = document.forms["formvalidation"]
-
-    // PEGANDO TERMOS E CONDIÇÕES
-    var terms = dada?.terms?.value
-
+    const data = document.forms["formvalidation"]
     // PEGANDO OS CAMPOS E SEUS RESPETIVOS VÁLORES
-    const nome = dada?.name?.value
-    const email = dada?.email?.value
-    const number = dada?.number?.value
-    const password = dada?.password?.value
-    const passwordConfirm = dada?.confirmPassword?.value
-
+    const fieldValues = {
+        nome: data?.name?.value,
+        email: data?.email?.value,
+        number: data?.number?.value,
+        password: data?.password?.value,
+        passwordConfirm: data?.confirmPassword?.value,
+    }
     // SEPARANDO A DATA EM: DD,MM E AAAA,  E CRIANDO CADA UM DELE 
     // COMO NÚMERO
-    const birthConfirm = {
-        birthDay: new Number(dada?.birthDay?.value),
-        birthGetMonth: new Number(dada?.birthMonth?.value),
-        birthGetYear: new Number(dada?.birthYear?.value),
-    }
-
-
+    const birthConfirm = data?.date?.value
+    const newDate = birthConfirm.split('-')
     // CRIANDO REGRAS DE CADA CAMPO DO FORMULÁRIO
     const rulesfieldsValidationForm = {
-        ruleTheName: nome.indexOf(" ") == -1,
-        ruleTheEmail: email.includes('@')
-            && email.includes('.com'),
-        rulesTheNumber: {
-            ruleTheNumberOne: !codigosDDD.includes(parseInt(number.substring(0, 2))),
-            ruleTheNumberTwo: number.length != 11,
+        name: { rule: fieldValues?.nome.indexOf(" ") !== -1, message: '' },
+        email: {
+            rule: fieldValues?.email.includes('@')
+                && fieldValues?.email.includes('.com'), message: ''
         },
+        number: {
+            rule: codigosDDD.includes(parseInt(fieldValues?.number.substring(0, 2)))
+                && fieldValues?.number.length === 11,
+            message: ''
+        },
+
         // DUAS REGRAS PARA O INPUT DATA DE NASCIMENTO
-        rulesFieldBirth: {
-            ruleTheBirthOne: birthConfirm?.birthDay > 31,
-            ruleTheBirthTwo: birthConfirm?.birthGetMonth > 12,
-            ruleTheBirthThree: birthConfirm?.birthGetYear < 1899,
-            ruleTheBirthFour: (today.getFullYear() < birthConfirm?.birthGetYear),
+        birth: {
+            rule: 1 > new Number(newDate[2]) < 31 &&
+                1 > new Number(newDate[1]) < 12 &&
+                1900 > new Number(newDate[0]) < 2023,
+                message: 'Por favor digite uma data válida!'
         },
         // TRÊS REGRAS PARA O INPUT PASSWORD
-        rulesFieldPassword: {
-            ruleThePasswordOne: password.match(/[0-9]/g) == null,
-            ruleThePasswordTwo: password.match(/[a-z]/g) == null,
-            ruleThePasswordThree: password.match(/[/@#$?]/g) == null,
+        password: {
+            rule: fieldValues?.password.match(/[0-9]/g) &&
+                fieldValues?.password.match(/[a-z]/g) &&
+                fieldValues?.password.match(/[@#$%]/g),
+            messsage: 'A senha deve conter uma letra mauscula, minuscula e um símbolo'
         },
-        ruleTheConfirmPassword: passwordConfirm != password
+        confirmPassword: {
+            rule: fieldValues?.passwordConfirm === fieldValues?.password,
+            message: 'A confirmação da senha deve ser igual à senha, :(!'
+        }
     }
-
+    console.log(rulesfieldsValidationForm)
+    return false
     // VERIFICANDO AS REGRAS
-    if (rulesfieldsValidationForm?.ruleTheName) {
-        const error = `Nome precisa ser nome completo, 
-            não apenas um nome(Ex.: Abimael Neto, não ${nome}`
-        errorMessages.push(`${error}`)
-    }
-    if (!rulesfieldsValidationForm?.ruleTheEmail) {
-        const error = `Email precisa ser um email válido, contendo @ e também .com,
-            não ${email}`
+    // if (rulesfieldsValidationForm?.Name) {
+    //     const error = `Nome precisa ser nome completo, 
+    //         não apenas um nome(Ex.: Abimael Neto, não ${nome}`
+    //     errorMessages.push(`${error}`)
+    // }
+    // if (!rulesfieldsValidationForm?.Email) {
+    //     const error = `Email precisa ser um email válido, contendo @ e também .com,
+    //         não ${email}`
 
-        errorMessages.push(`${error}`)
-    }
-    if (rulesfieldsValidationForm?.rulesTheNumber?.ruleTheNumberOne) {
-        const error = `O telefone deve conter DDD, ${number} não é um DDD!`
-        errorMessages.push(`${error}`)
-    } else if (rulesfieldsValidationForm?.rulesTheNumber?.ruleTheNumberTwo) {
-        const error = `Telefone deve ter 11 números`
-        errorMessages.push(`${error}`)
-    }
-    if (rulesfieldsValidationForm?.rulesFieldBirth?.ruleTheBirthOne) {
-        const error = `o campo DD não pode ser maior que 31`
-        errorMessages.push(`${error}`)
-    }
-    if (rulesfieldsValidationForm?.rulesFieldBirth?.ruleTheBirthTwo) {
-        const error = `o campo MM não pode ser maior que 12`
-        errorMessages.push(`${error}`)
-    }
-    if (rulesfieldsValidationForm?.rulesFieldBirth?.ruleTheBirthThree) {
-        const error = `O ano não pode ser menor que 1900`
-        errorMessages.push(`${error}`)
-    }
-    if (rulesfieldsValidationForm?.rulesFieldBirth?.ruleTheBirthFour) {
-        const error = `O ano não pode ser maior que atual!`
-        errorMessages.push(`${error}`)
-    }
-    if (rulesfieldsValidationForm?.rulesFieldPassword?.ruleThePasswordOne) {
-        const error = `O campo senha pelo menos um número`
+    //     errorMessages.push(`${error}`)
+    // }
+    // if (rulesfieldsValidationForm?.rulesTheNumber?.NumberOne) {
+    //     const error = `O telefone deve conter DDD, ${number} não é um DDD!`
+    //     errorMessages.push(`${error}`)
+    // } else if (rulesfieldsValidationForm?.rulesTheNumber?.NumberTwo) {
+    //     const error = `Telefone deve ter 11 números`
+    //     errorMessages.push(`${error}`)
+    // }
+    // if (rulesfieldsValidationForm?.rulesFieldBirth?.BirthOne) {
+    //     const error = `o campo DD não pode ser maior que 31`
+    //     errorMessages.push(`${error}`)
+    // }
+    // if (rulesfieldsValidationForm?.rulesFieldBirth?.BirthTwo) {
+    //     const error = `o campo MM não pode ser maior que 12`
+    //     errorMessages.push(`${error}`)
+    // }
+    // if (rulesfieldsValidationForm?.rulesFieldBirth?.BirthThree) {
+    //     const error = `O ano não pode ser menor que 1900`
+    //     errorMessages.push(`${error}`)
+    // }
+    // if (rulesfieldsValidationForm?.rulesFieldBirth?.BirthFour) {
+    //     const error = `O ano não pode ser maior que atual!`
+    //     errorMessages.push(`${error}`)
+    // }
+    // if (rulesfieldsValidationForm?.rulesFieldPassword?.PasswordOne) {
+    //     const error = `O campo senha pelo menos um número`
 
-        errorMessages.push(`${error}`)
-    }
-    if (rulesfieldsValidationForm?.rulesFieldPassword?.ruleThePasswordTwo) {
-        const error = `O campo senha deve conter uma letra minuscula pelo menos`
-        errorMessages.push(`${error}`)
-    }
-    if (rulesfieldsValidationForm?.rulesFieldPassword?.ruleThePasswordThree) {
-        const error = (`O campo senha deve conter 1 simbolo pelo menos`)
+    //     errorMessages.push(`${error}`)
+    // }
+    // if (rulesfieldsValidationForm?.rulesFieldPassword?.PasswordTwo) {
+    //     const error = `O campo senha deve conter uma letra minuscula pelo menos`
+    //     errorMessages.push(`${error}`)
+    // }
+    // if (rulesfieldsValidationForm?.rulesFieldPassword?.PasswordThree) {
+    //     const error = (`O campo senha deve conter 1 simbolo pelo menos`)
 
-        errorMessages.push(`${error}`)
-    }
-    if (rulesfieldsValidationForm?.ruleTheConfirmPassword) {
-        const error = `A confirmação da senha deve ser igual á senha`
-        errorMessages.push(`${error}`)
-    }
-
+    //     errorMessages.push(`${error}`)
+    // }
+    // if (rulesfieldsValidationForm?.ConfirmPassword) {
+    //     const error = `A confirmação da senha deve ser igual á senha`
+    //     errorMessages.push(`${error}`)
+    // }
 
     // VALIDÁÇÃO FINAL, VERIFICANDO SE A ARRAY DE ERROR 
     // TEM ALGO OU NÃO.
     if (errorMessages.length == 0) {
-        // SE TEM
+        // SE NÃO TEM
         alert('Formulário válido')
         return true
     } else {
-        // SE NÃO TEM
-        alert(`${errorMessages.map((item)=> {
+        // SE TEM
+        alert(`${errorMessages.map((item) => {
             return item
         })}`)
         button.style.display = 'none'
         buttonError.style.display = 'flex'
         setTimeout(() => {
             buttonError.style.display = 'none'
-            button.style.display = 'flex' 
+            button.style.display = 'flex'
             errorMessages = []
         }, 4000)
         return false
